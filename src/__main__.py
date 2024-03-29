13b19eb886ccb67689ba8a13125b7b58e7367933
Samuel Colvin <s@muelcolvin.com>
2024-03-29 16:14:20 -0600

    set auto-tracing min-duration to 1ms

--- src/__main__.py
+++ src/__main__.py
@@ -6,7 +6,8 @@ import logfire
 
 service = os.getenv('SERVICE')
 services = 'webui', 'tiling', 'worker', 'spider'
-logfire.install_auto_tracing(modules=[f'src.{s}' for s in services], min_duration=0.1)
+# min duration is 1ms
+logfire.install_auto_tracing(modules=[f'src.{s}' for s in services], min_duration=0.001)
 if service is None:
     print('SERVICE environment variable not set')
     print('Available services:', ', '.join(services))
