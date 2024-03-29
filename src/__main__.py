355b9c513db349aaef10a19040f874740522d44f
Samuel Colvin <s@muelcolvin.com>
2024-03-29 16:23:16 -0600

    disable spider auto-tracing

--- src/__main__.py
+++ src/__main__.py
@@ -6,8 +6,8 @@ import logfire
 
 service = os.getenv('SERVICE')
 services = 'webui', 'tiling', 'worker', 'spider'
-# min duration is 1ms
-logfire.install_auto_tracing(modules=[f'src.{s}' for s in services], min_duration=0.001)
+# min duration is 1ms, spider isn't helped by auto tracing
+logfire.install_auto_tracing(modules=[f'src.{s}' for s in services if s != 'spider'], min_duration=0.001)
 if service is None:
     print('SERVICE environment variable not set')
     print('Available services:', ', '.join(services))
