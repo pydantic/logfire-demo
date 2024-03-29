39f707d3b6dfd8ac586c2461df425fcf72b5a0e4
Samuel Colvin <s@muelcolvin.com>
2024-03-29 16:00:57 -0600

    enabling auto-tracing

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,12 @@
 import os
 import importlib
 
+import logfire
+
+
 service = os.getenv('SERVICE')
 services = 'webui', 'tiling', 'worker', 'spider'
+logfire.install_auto_tracing(modules=[f'src.{s}' for s in services], min_duration=0.1)
 if service is None:
     print('SERVICE environment variable not set')
     print('Available services:', ', '.join(services))
--- src/spider/__init__.py
+++ src/spider/__init__.py
@@ -6,7 +6,7 @@ from httpx import AsyncClient
 
 from . import tasks
 
-logfire.configure(service_name='spider')
+logfire.configure(send_to_logfire='if-token-present', service_name='spider')
 
 
 def run():
@@ -14,16 +14,19 @@ def run():
 
 
 async def arun():
-    async with AsyncClient() as client:
-        while True:
-            match random.choice(('get_homepage', 'get_cities', 'llm_query')):
-                case 'get_homepage':
-                    await tasks.get_homepage(client)
-                case 'get_cities':
-                    await tasks.get_cities(client)
-                case 'llm_query':
-                    await tasks.llm_query(client)
+    try:
+        async with AsyncClient() as client:
+            while True:
+                match random.choice(('get_homepage', 'get_cities', 'llm_query')):
+                    case 'get_homepage':
+                        await tasks.get_homepage(client)
+                    case 'get_cities':
+                        await tasks.get_cities(client)
+                    case 'llm_query':
+                        await tasks.llm_query(client)
 
-            delay = int(15 + random.random() * 45)
-            with logfire.span(f'waiting {delay}s', delay=delay):
-                await asyncio.sleep(delay)
+                delay = int(15 + random.random() * 45)
+                with logfire.span(f'waiting {delay}s', delay=delay):
+                    await asyncio.sleep(delay)
+    except asyncio.CancelledError:
+        logfire.info('Cancelled')
