0b70bd23399637ea5b3461aaf5e47abd429f0127
Samuel Colvin <s@muelcolvin.com>
2024-09-30 22:22:16 +0100

    increase min duration

--- src/__main__.py
+++ src/__main__.py
@@ -7,7 +7,7 @@ import logfire
 service = sys.argv[1] if len(sys.argv) == 2 else os.getenv('SERVICE')
 services = 'webui', 'tiling', 'worker', 'spider'
 # min duration is 10ms, spider isn't helped by auto tracing
-logfire.install_auto_tracing(modules=[f'src.{s}' for s in services if s != 'spider'], min_duration=0.01)
+logfire.install_auto_tracing(modules=[f'src.{s}' for s in services if s != 'spider'], min_duration=0.1)
 if service is None:
     print('service argument variable not provided', file=sys.stderr)
     print('Available services:', ', '.join(services), file=sys.stderr)
