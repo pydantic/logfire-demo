9beac5bb3e44cde3c817f9dcc79ad87e444620a3
Hasan Ramezani <hasan.r67@gmail.com>
2025-02-13 15:13:59 +0330

    Refactore slack message webhook

---
+++ src/common/db/slack.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from asyncpg import Connection
+
+from ..embeddings import hash_text
+
+
+async def create_slack_message(
+    conn: Connection,
+    channel: str,
+    author: str,
+    message_id: str,
+    event_ts: str,
+    parent_event_ts: str | None,
+    text: str,
+    ts: datetime,
+    embedding: list[list[float]],
+) -> None:
+    """Create a new slack message in the database"""
+    embedding_str = '[' + ','.join(map(str, embedding)) + ']'
+    await conn.execute(
+        """
+        INSERT INTO slack_messages (channel, author, message_id, event_ts, parent_event_ts, text, hash, ts, embedding)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+        """,
+        channel,
+        author,
+        message_id,
+        event_ts,
+        parent_event_ts,
+        text,
+        hash_text(text),
+        ts,
+        embedding_str,
+    )
