b0ec28b36711c262602c96f3f41ed1258531acef
Hasan Ramezani <hasan.r67@gmail.com>
2025-02-14 18:51:53 +0330

    Fix github db functions (#37)

--- src/common/db/github.py
+++ src/common/db/github.py
@@ -23,7 +23,7 @@ async def create_github_content(
     await conn.execute(
         """
         INSERT INTO github_contents (project, source, content_id, external_reference, text, event_ts, embedding)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
         """,
         project,
         source,
@@ -64,7 +64,7 @@ async def update_github_content(
     embedding_str = '[' + ','.join(map(str, embedding)) + ']'
     await conn.execute(
         """
-        UPDATE github_contents SET text=$1, embedding=$3 WHERE project=$4 AND source=$5 AND content_id=$6
+        UPDATE github_contents SET text=$1, embedding=$2 WHERE project=$3 AND source=$4 AND content_id=$5
         """,
         text,
         embedding_str,
