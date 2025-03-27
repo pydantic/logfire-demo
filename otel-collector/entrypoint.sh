f00d82a8a430527c648ea5039f9303049574a4b9
Hasan Ramezani <hasan.r67@gmail.com>
2025-03-27 13:03:40 +0330

    Fix otel collector config

--- otel-collector/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e  # Exit if any command fails
-
-# Use envsubst to replace environment variables in the config file
-envsubst < /etc/otel-collector-config.yaml > /tmp/otel-collector-config.yaml
-
-# Start the OpenTelemetry Collector with the new config
-exec /otelcol-contrib --config /tmp/otel-collector-config.yaml
